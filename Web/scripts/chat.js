9b20db0d8608bf93ff23cacd36d27fcbc387a7f0
= <->
2018-10-28 01:07:48 +0100

    Talk Talk Talk!!!1

--- Web/scripts/chat.js
+++ Web/scripts/chat.js
@@ -14,11 +14,62 @@ $(document).ready(function(){
             $(document).scrollTop($(document).height());
 
             //todo send text to server by room id
+            $(document).ready(function() {
+            $.ajax({
+                url: "guy's url"
+            }).then(function(data) {
+                //successful then what todo
+                $('.greeting-id').append(data.id);
+                $('.greeting-content').append(data.content);
+            });
+        });
         }
     });
 
     window.setInterval(function(){
         console.log("let me sleeppppp");
-    }, 1000);
+        //todo fetch messages from server
+        var trackingJSON = '{mockup data}';
+        var postUrl =  "guy's url";
+
+        $.ajax({
+            type: "POST",
+            url: postUrl,
+            contentType: "application/json",
+            data: trackingJSON,
+            beforeSend: function() { $.mobile.showPageLoadingMsg("b", "Loading...", true) },
+            complete: function() { $.mobile.hidePageLoadingMsg() },
+            success: function(data) { /*todo update chat page*/ },
+            error: function(data) { console.log("error: 39381") },
+            dataType: 'json'
+        });
+    }, 500);
 });
 
+
+
+// EXAMPLE GET
+// $(document).ready(function() {
+//     $.ajax({
+//         url: "http://rest-service.guides.spring.io/greeting"
+//     }).then(function(data) {
+//         $('.greeting-id').append(data.id);
+//         $('.greeting-content').append(data.content);
+//     });
+// });
+
+// EXAMPLE POST
+// var trackingJSON = JSON.stringify(tracking_data);
+// var urlAjax =  "http://localhost:7001/ds/resources/track/" + trackid;
+//
+// $.ajax({
+//     type: "POST",
+//     url: urlAjax,
+//     contentType: "application/json",
+//     data: trackingJSON,
+//     beforeSend: function() { $.mobile.showPageLoadingMsg("b", "Loading...", true) },
+//     complete: function() { $.mobile.hidePageLoadingMsg() },
+//     success: function(data) { alert("ajax worked"); },
+//     error: function(data) {alert("ajax error"); },
+//     dataType: 'json'
+// });
